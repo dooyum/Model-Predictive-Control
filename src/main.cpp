b7f43e85fd66b18f4f3a5dbbfca324073732cd6c
dooyum <dooyumjmalu@gmail.com>
2018-04-29 17:36:54 -0700

    Working once latency is removed

--- src/main.cpp
+++ src/main.cpp
@@ -91,6 +91,16 @@ int main() {
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+          double acc = j[1]["throttle"];
+          double delta = j[1]["steering_angle"];
+
+          // Factor in a 100ms latency delay in actuation.
+          double latency = 0.1; // 100ms.
+          double Lf = 2.67;
+          // px, py, psi and v 100ms into the future.
+          px += v * cos(psi) * latency;
+          py += v * sin(psi) * latency;
+          psi = psi - (v * delta * latency) * Lf;
 
           vector<double> vehicle_ptsx = {};
           vector<double> vehicle_ptsy = {};
@@ -107,6 +117,7 @@ int main() {
             vehicle_ptsx.push_back(vehicle_x);
             vehicle_ptsy.push_back(vehicle_y);
           }
+
           Eigen::VectorXd ptsx_eigen = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(vehicle_ptsx.data(), vehicle_ptsx.size());
           Eigen::VectorXd ptsy_eigen = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(vehicle_ptsy.data(), vehicle_ptsy.size());
 
@@ -116,36 +127,32 @@ int main() {
           double cte = polyeval(coeffs, 0);
           // Psi is always 0 w.r.t. the vehicle coordniates.
           // x is 0 w.r.t the vehicle coordinates, so all derivate values of the polynomial with an x can be removed.
-          // i.e. C2 * x + C3 * x^2
+          // i.e. C2 * x + C3 * x^2 = 0
           double epsi = -atan(coeffs[1]);
-          std::cout << "VEE = " << v * 0.447 << std::endl;
 
           Eigen::VectorXd state(6);
-          // The vehicle state w.r.t the vehicle is positioned at [x,y] = [0,0] at a 0 radian angle.
+          // The vehicle state w.r.t the vehicle is positioned at [x,y] = [0,0] at a 0 degree angle.
           state << 0, 0, 0, v, cte, epsi;
           auto vars = mpc.Solve(state, coeffs);
-
-          /*
-          * TODO: Calculate steering angle and throttle using MPC.
-          *
-          * Both are in between [-1, 1].
-          *
-          */
-          double steer_value = vars[6];
-          double throttle_value = vars[7];
+          double steer_value = vars[0];
+          double throttle_value = vars[1];
 
           json msgJson;
-          // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
-          // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
+          // Divide by deg2rad(25) to bring values between [-1, 1].
           msgJson["steering_angle"] = steer_value / deg2rad(25);
-          msgJson["throttle"] = throttle_value; // 1m/s = 2.23694mph
+          msgJson["throttle"] = throttle_value;
 
           //Display the MPC predicted trajectory 
-          vector<double> mpc_x_vals = vehicle_ptsx;
-          vector<double> mpc_y_vals = vehicle_ptsy;
+          vector<double> mpc_x_vals;
+          vector<double> mpc_y_vals;
 
-          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
-          // the points in the simulator are connected by a Green line
+          for (unsigned int i = 2; i < vars.size(); i++) {
+            if (i % 2 == 0) {
+              mpc_x_vals.push_back(vars[i]);
+            } else {
+              mpc_y_vals.push_back(vars[i]);
+            }
+          }
 
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
@@ -154,25 +161,21 @@ int main() {
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
-          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
-          // the points in the simulator are connected by a Yellow line
           // Number of future way points to show.
-          double num_way_points = 25;
-          double point_gap = 2.5;
+          double num_way_points = 15;
+          double point_gap = 1.5;
           for (unsigned int x_point = 0; x_point < num_way_points; x_point++) {
             next_x_vals.push_back(x_point * point_gap);
             next_y_vals.push_back(polyeval(coeffs, x_point * point_gap));
           }
-
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
-
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           // Latency
           // The purpose is to mimic real driving conditions where
-          // the car does actuate the commands instantly.
+          // the car doesn't actuate the commands instantly.
           //
           // Feel free to play around with this value but should be to drive
           // around the track with 100ms latency.
